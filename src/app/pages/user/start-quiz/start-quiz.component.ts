e37c7bf36c164630daea10d8585a51a50c491854
sainivinit <vinitsaini357@gmail.com>
2021-06-28 00:32:59 +0530

    Submit test and calculate answer,attempted etc

--- src/app/pages/user/start-quiz/start-quiz.component.ts
+++ src/app/pages/user/start-quiz/start-quiz.component.ts
@@ -2,6 +2,7 @@ import { LocationStrategy } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { QuestionService } from 'src/app/services/question.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-start-quiz',
@@ -17,26 +18,58 @@ export class StartQuizComponent implements OnInit {
   qid=0;
   question:any;
 
+  marksGot=0;
+  correctAnswer=0;
+  attempted=0;
+
+  isSubmit=false;
+
+
+
+
+
   ngOnInit(): void {
 
    this.qid=this._rout.snapshot.params.qid;
 
      this.blockBackButton();
 
-     this._questions.getQuestionsByQuizforUser(this.qid).subscribe((data)=>{
+     this.loadQuiz();
+
+
+
+
+
+
+
+
+
+
+  }
+
+  /**
+   * loadQuiz
+   */
+  public loadQuiz() {
+    this._questions.getQuestionsByQuizforUser(this.qid).subscribe((data)=>{
 
 
 
       this.question=data;
-      console.log(data)
+      
+     this.question.forEach((q:any) => {
+      q['givenAnswer']='';
+    });
+ 
+    
 
      },
      
      (error)=>{
       console.log(error)
      })
-  }
 
+  }
 
   blockBackButton(){
     history.pushState(null,'',location.href);
@@ -46,5 +79,46 @@ export class StartQuizComponent implements OnInit {
     })
   }
 
+
+
+  submitQuiz(){
+    Swal.fire({
+      title:'Do you want to start Submit ?',
+      showCancelButton:true,
+      confirmButtonText:"Yes, Submit",
+      icon:'info'
+    }).then((result)=>{
+      if(result.isConfirmed){
+        this.isSubmit=true;
+        this.question.forEach((q:any) => {
+          
+
+          if(q.givenAnswer==q.answer){
+            this.correctAnswer++;
+
+            let marksCurrent=this.question[0].quiz.maxMax/this.question.length;
+
+            this.marksGot+=marksCurrent;
+
+            
+          }
+
+          if(q.givenAnswer.trim()!=''){
+            this.attempted++;
+          }
+
+          
+          
+          
+        });
+
+        console.log("correct answer"+this.correctAnswer);
+          console.log("marks got"+this.marksGot);
+          console.log(this.attempted);
+          
+        
+      }
+    })
+  }
 
 }
